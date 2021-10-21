41a505c26fff93f24649a77cdd55c8115b6c51b8
muhammadsafna <safnawhz@gmail.com>
2021-10-21 10:33:35 +0700

    Tugas 4

---
+++ Praktek6/ListBiograpy/app/src/main/java/com/example/listbiograpy/NamaData.java
@@ -0,0 +1,2 @@
+package com.example.listbiograpy;public class NamaData {
+}
---
+++ Praktek6/app/src/main/java/com/example/listbiography/DetailTokoh.java
@@ -0,0 +1,14 @@
+package com.example.listbiography;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailTokoh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_tokoh);
+    }
+}
